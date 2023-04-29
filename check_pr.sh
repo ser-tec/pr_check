2e426aa130a45fc3c6741092682705774e34020b
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-04-29 18:34:28 +0200

    Update check_pr.sh

--- check_pr.sh
+++ check_pr.sh
@@ -3,29 +3,18 @@
 pr_path="/opt/odoo16/addons/PR"
 l10n_italy_path="/opt/odoo16/addons/OCA/l10n-italy"
 
-pr_list=$(ls -d $pr_path/* 2>/dev/null)
-l10n_italy_list=$(ls -d $l10n_italy_path/* 2>/dev/null)
+# Controlla se entrambi i percorsi esistono
+if [ -d "$pr_path" ] && [ -d "$l10n_italy_path" ]; then
+    # Elenca le cartelle in entrambi i percorsi
+    pr_folders=$(find "$pr_path" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
+    l10n_italy_folders=$(find "$l10n_italy_path" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 
-for folder in $pr_list; do
-  pr_array+=("$folder")
-done
-
-for folder in $l10n_italy_list; do
-  l10n_italy_array+=("$folder")
-done
-
-i=0
-
-for folder in $pr_list; do
- if [[ " ${l10n_italy_array[*]} " =~ " ${pr_array[i]} " ]]; then
-   elementi_comuni+=($pr_array);
-   i++
- fi
-done
-
-echo ${elementi_comuni[*]}
-
-va tolto il path iniziale per il confronto
-
-#echo ${pr_array[2]}
-#echo ${l10n_italy_array[3]}
+    # Trova e stampa le cartelle con lo stesso nome in entrambi i percorsi
+    for folder in $pr_folders; do
+        if [[ $l10n_italy_folders =~ (^|[[:space:]])"$folder"($|[[:space:]]) ]]; then
+            echo "Cartella con lo stesso nome trovata: $folder"
+        fi
+    done
+else
+    echo "Uno o entrambi i percorsi non esistono. Verifica i percorsi e riprova."
+fi
