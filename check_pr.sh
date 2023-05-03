73895e52a81b7b7fbd26d2cf864bc606de1d58b5
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-05-03 08:33:24 +0200

    Update check_pr.sh

--- check_pr.sh
+++ check_pr.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-pr_path="/opt/odoo16/addons/PR"
-l10n_italy_path="/opt/odoo16/addons/OCA/l10n-italy"
-
 # Controlla se entrambi i percorsi esistono
-if [ -d "$pr_path" ] && [ -d "$l10n_italy_path" ]; then
+if [ -d "$PR" ] && [ -d "$OCA" ]; then
     # Elenca le cartelle in entrambi i percorsi
-    pr_folders=$(find "$pr_path" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
-    l10n_italy_folders=$(find "$l10n_italy_path" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
+    pr_folders=$(find "$PR" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
+    oca_folders=$(find "$OCA" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 
     # Trova e stampa le cartelle con lo stesso nome in entrambi i percorsi
     for folder in $pr_folders; do
-        if [[ $l10n_italy_folders =~ (^|[[:space:]])"$folder"($|[[:space:]]) ]]; then
+        if [[ $oca_folders =~ (^|[[:space:]])"$folder"($|[[:space:]]) ]]; then
             echo "Cartella con lo stesso nome trovata: $folder"
         fi
     done
