6de5af614416a2239276f249a04ff68034cb747f
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-05-03 08:44:15 +0200

    Update check_pr.sh

--- check_pr.sh
+++ check_pr.sh
@@ -10,11 +10,25 @@ if [ -d "$PR" ] && [ -d "$OCA" ]; then
     oca_folders=$(find "$OCA" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 
     # Trova e stampa le cartelle con lo stesso nome in entrambi i percorsi
+    duplicate_folders=""
     for folder in $pr_folders; do
         if [[ $oca_folders =~ (^|[[:space:]])"$folder"($|[[:space:]]) ]]; then
             echo "Cartella con lo stesso nome trovata: $folder"
+            duplicate_folders+="\n$folder"
         fi
     done
+    
+# Invia la lista delle cartelle duplicate via e-mail solo se $EMAIL non è vuota
+if [ -n "$duplicate_folders" ]; then
+    if [ -n "$EMAIL" ]; then
+        echo -e "Cartelle duplicate trovate:$duplicate_folders" | mail -s "Report cartelle duplicate" $EMAIL
+    else
+        echo "L'indirizzo e-mail non è impostato. Impossibile inviare il report."
+    fi
+else
+    echo "Nessuna cartella duplicata trovata."
+fi
+    
 else
     echo "Uno o entrambi i percorsi non esistono. Verifica i percorsi e riprova."
 fi
